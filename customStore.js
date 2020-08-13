5195b6202b58c00c8411ade7d37528c980d332a9
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2020-08-13 13:43:22 +0100

    setting up webpack, creation of customStore file

---
+++ customStore.js
@@ -0,0 +1,12 @@
+const createStore = (reducer) => {
+    let state; //the state of the store. updated be the reducer function
+
+    /**
+     * this returns the current state of the store
+     * @param   none
+     * @return  object  the current state of the store
+     */
+    const getState = () => {
+        return state;
+    }
+}
